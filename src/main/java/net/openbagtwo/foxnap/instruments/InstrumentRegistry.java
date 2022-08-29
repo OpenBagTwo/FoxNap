c8ab5adb3951248eef18c2f93c6b35c17a556310
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2022-08-29 08:54:26 -0400

    Further success
    This plays the right sound and everything (still gotta use the /give command with NBT data though)

--- src/main/java/net/openbagtwo/foxnap/instruments/InstrumentRegistry.java
+++ src/main/java/net/openbagtwo/foxnap/instruments/InstrumentRegistry.java
@@ -1,5 +1,6 @@
 package net.openbagtwo.foxnap.instruments;
 
+import net.minecraft.item.GoatHornItem;
 import net.minecraft.item.Item;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
@@ -27,11 +28,11 @@ public class InstrumentRegistry {
    *                       event)
    * @return the fully instantiated and registered instrument
    */
-  public static OrchestraInstrument registerInstrument(String instrumentName) {
+  public static GoatHornItem registerInstrument(String instrumentName) {
     return Registry.register(
         Registry.ITEM,
         new Identifier(FoxNap.MOD_ID, instrumentName),
-        new OrchestraInstrument(
+        new LiterallyJustAGoatHorn(
             registerInstrumentSound(instrumentName),
             20 * INSTRUMENTS.get(instrumentName))
     );
@@ -48,11 +49,6 @@ public class InstrumentRegistry {
    * @return A list of fully instantiated and registered instruments
    */
   public static List<Item> init() {
-    Registry.register(
-        Registry.ITEM,
-        new Identifier(FoxNap.MOD_ID, "literally_just_a_goat_horn"),
-        new LiterallyJustAGoatHorn()
-    );
     ArrayList<Item> instruments = new ArrayList<>();
     for (String instrument : INSTRUMENTS.keySet()) {
       instruments.add(registerInstrument(instrument));
