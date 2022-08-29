18b7e34dad48c49b0de83462ad91af4a0af3f4a9
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2022-08-29 08:19:26 -0400

    Fix texture path, work on orientation...
    ... and cry because the use action isn't actually changing the pose.

--- src/main/java/net/openbagtwo/foxnap/instruments/OrchestraInstrument.java
+++ src/main/java/net/openbagtwo/foxnap/instruments/OrchestraInstrument.java
@@ -1,7 +1,6 @@
 package net.openbagtwo.foxnap.instruments;
 
 import net.minecraft.entity.player.PlayerEntity;
-
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -11,7 +10,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
-
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 
@@ -35,24 +33,6 @@ public class OrchestraInstrument extends Item {
     this.cooldown = cooldown;
   }
 
-  public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        /*
-       TODO:
-             - implement handed instruments (can only be held in right or left hand)
-             - implement two-handed instruments (require other hand to be empty)
-             - implement two-item instruments (e.g. violin + bow)
-         */
-    ItemStack itemStack = user.getStackInHand(hand);
-    user.setCurrentHand(hand);
-    playSound(world, user, this.soundEvent);
-    user.getItemCooldownManager().set(this, this.cooldown);
-    return TypedActionResult.consume(itemStack);
-  }
-
-  public UseAction getUseAction(ItemStack stack) {
-    return UseAction.TOOT_HORN;
-  }
-
   private static void playSound(World world, PlayerEntity player, SoundEvent soundEvent) {
     world.playSoundFromEntity(
         player,
@@ -67,5 +47,26 @@ public class OrchestraInstrument extends Item {
         player.getPos(),
         GameEvent.Emitter.of(player)
     );
+  }
+
+  @Override
+  public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        /*
+       TODO:
+             - implement handed instruments (can only be held in right or left hand)
+             - implement two-handed instruments (require other hand to be empty)
+             - implement two-item instruments (e.g. violin + bow)
+         */
+    ItemStack itemStack = user.getStackInHand(hand);
+    user.setCurrentHand(hand);
+
+    playSound(world, user, this.soundEvent);
+    user.getItemCooldownManager().set(this, this.cooldown);
+    return TypedActionResult.consume(itemStack);
+  }
+
+  @Override
+  public UseAction getUseAction(ItemStack stack) {
+    return UseAction.TOOT_HORN;
   }
 }
