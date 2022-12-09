c78e76ef291785bcda52af0be6173c51a3feaba9
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2022-12-09 13:47:08 -0500

    Don't show placeholder discs in creative inventory

--- src/main/java/net/openbagtwo/foxnap/discs/Disc.java
+++ src/main/java/net/openbagtwo/foxnap/discs/Disc.java
@@ -14,17 +14,22 @@ public class Disc extends MusicDiscItem {
   /**
    * Create a new music disc
    *
-   * @param comparatorOutput the output signal a comparator should read from a jukebox with this
-   *                         disc loaded
-   * @param sound            the sound (track) a jukebox with this disc loaded should play
-   * @param trackLength      The length of the track in seconds. This value is currently only used
-   *                         by Allays (presumably to determine when to stop dancing and duping?).
+   * @param comparatorOutput  the output signal a comparator should read from a jukebox with this
+   *                          disc loaded
+   * @param sound             the sound (track) a jukebox with this disc loaded should play
+   * @param trackLength       The length of the track in seconds. This value is currently only used
+   *                          by Allays (presumably to determine when to stop dancing and duping?).
+   * @param creativeInventory whether the disc should appear in the creative inventory
    */
-  public Disc(int comparatorOutput, SoundEvent sound, int trackLength) {
-    super(comparatorOutput, sound, generateSettings(), trackLength);
+  public Disc(int comparatorOutput, SoundEvent sound, int trackLength, boolean creativeInventory) {
+    super(comparatorOutput, sound, generateSettings(creativeInventory), trackLength);
   }
 
-  private static Item.Settings generateSettings() {
-    return new Item.Settings().rarity(Rarity.RARE).maxCount(1).group(ItemGroup.MISC);
+  private static Item.Settings generateSettings(boolean creativeInventory) {
+    Item.Settings settings = new Item.Settings().rarity(Rarity.RARE).maxCount(1);
+    if (creativeInventory) {
+      settings = settings.group(ItemGroup.MISC);
+    }
+    return settings;
   }
 }
