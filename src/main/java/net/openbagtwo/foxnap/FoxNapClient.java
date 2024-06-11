4f1790f5d265bbe9fe1161b503eb6b84488eb5a7
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-06-11 09:51:31 -0400

    Switch to using ModelLoadingPlugin for placeholder disc rendering

--- src/main/java/net/openbagtwo/foxnap/FoxNapClient.java
+++ src/main/java/net/openbagtwo/foxnap/FoxNapClient.java
@@ -1,36 +1,20 @@
 package net.openbagtwo.foxnap;
 
 import static net.openbagtwo.foxnap.FoxNap.LOGGER;
-import static net.openbagtwo.foxnap.FoxNap.MOD_ID;
 import static net.openbagtwo.foxnap.FoxNap.MOD_NAME;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.openbagtwo.foxnap.config.Config;
+import net.openbagtwo.foxnap.discs.DiscRenderer;
 
 public class FoxNapClient implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    Identifier placeholder = Identifier.of(MOD_ID, "item/placeholder");
     Config config = Config.loadConfiguration();
 
-    ModelLoadingRegistry.INSTANCE.registerResourceProvider(
-        manager -> (resourceId, context) -> {
-          for (
-              int i = config.getNumDiscs();
-              i < config.getMaximumNumberOfDiscs();
-              i++
-          ) {
-            Identifier matchMe = new Identifier(MOD_ID, String.format("item/track_%d", i + 1));
-            if (resourceId.equals(matchMe)) {
-              return context.loadModel(placeholder);
-            }
-          }
-          return null;
-        }
-    );
+    ModelLoadingPlugin.register(new DiscRenderer(config));
 
     LOGGER.info(MOD_NAME + " Client Initialization Complete");
   }
---
+++ src/main/java/net/openbagtwo/foxnap/discs/DiscRenderer.java
@@ -0,0 +1,35 @@
+package net.openbagtwo.foxnap.discs;
+
+import static net.openbagtwo.foxnap.FoxNap.MOD_ID;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.minecraft.util.Identifier;
+import net.openbagtwo.foxnap.config.Config;
+
+@Environment(EnvType.CLIENT)
+public class DiscRenderer implements ModelLoadingPlugin {
+
+  private final int placeholderStart;
+  private final int maxNumDiscs;
+
+  public DiscRenderer(Config config) {
+    this.placeholderStart = config.getNumDiscs();
+    this.maxNumDiscs = config.getMaximumNumberOfDiscs();
+  }
+
+  @Override
+  public void onInitializeModelLoader(Context pluginContext) {
+    Identifier placeholder = Identifier.of(MOD_ID, "item/placeholder");
+    pluginContext.modifyModelOnLoad().register((original, context) -> {
+      for (int i = this.placeholderStart; i < this.maxNumDiscs; i++) {
+        Identifier matchMe = Identifier.of(MOD_ID, String.format("item/track_%d", i + 1));
+        if (context.resourceId().equals(matchMe)) {
+          return context.getOrLoadModel(placeholder);
+        }
+      }
+      return original;
+    });
+  }
+}
